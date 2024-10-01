92ede3cc9a5aa2104b0c186cb26a3d8e421e8935
khmilevoi <khmilevoi@gmail.com>
2024-10-01 15:46:29 +0200

    add plugins to the build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,8 @@
+import * as fs from "fs";
 import { dts } from "rollup-plugin-dts";
 import { swc } from "rollup-plugin-swc3";
+
+const plugins = fs.readdirSync("./src/plugins");
 
 export default [
   {
@@ -16,9 +19,28 @@ export default [
     ],
     plugins: [swc()],
   },
+  ...plugins.map((plugin) => ({
+    input: `src/plugins/${plugin}/index.ts`,
+    output: [
+      {
+        file: `dist/plugins/${plugin}/index.cjs`,
+        format: "cjs",
+      },
+      {
+        file: `dist/plugins/${plugin}/index.mjs`,
+        format: "esm",
+      },
+    ],
+    plugins: [swc()],
+  })),
   {
     input: "src/index.ts",
     output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
   },
+  ...plugins.map((plugin) => ({
+    input: `src/plugins/${plugin}/index.ts`,
+    output: [{ file: `dist/plugins/${plugin}/index.d.ts`, format: "es" }],
+    plugins: [dts()],
+  })),
 ];
